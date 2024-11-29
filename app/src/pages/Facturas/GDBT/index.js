af78ede8e209dffbc35c133a90d404a4beeda6d7
vicente443 <al066219@uacam.mx>
2024-11-29 13:55:11 -0600

    Implementación de tablas facruras

--- app/src/pages/Facturas/GDBT/index.js
+++ app/src/pages/Facturas/GDBT/index.js
@@ -7,13 +7,15 @@ import { Link } from "react-router-dom";
 import { Button } from "@mui/material";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import Tooltip from "@mui/material/Tooltip";
+import '../GDBT/./style.css'
 
 const Tabla_GDBT = () => {
   return (
+    
     <div className="w-100 right-content">
       <div className="card">
         <div className="table-header d-flex justify-content-end align-items-center mt-2 mb-3 p-2">
-          <Link to="/agregar_tarifa">{ /* editar para agregar tarifa */}
+          <Link to="/agregar_Factura">{ /* editar para agregar tarifa */}
             <Button
               variant="contained"
               className=" mx-2"
@@ -25,25 +27,37 @@ const Tabla_GDBT = () => {
           </Link>
         </div>
 
+
         <div className="d-flex justify-content-center align-items-center">
           <div className="table-container">
             <table className="styled-table text-center">
               <thead>
                 <tr>
                   <th colSpan="3" className="table-title text-center">
-                    Tabla de Tarifas
+                    Facturas 2019
                   </th>
                 </tr>
                 <tr>
-                  <th className="table-title text-center">Tarifa</th>
-                  <th className="table-title text-center">Descripción</th>
+                  <th className="table-title text-center">Mes</th>
+                  <th className="table-title text-center">Archivo</th>
                   <th className="table-title text-center">Acciones</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td>PDBT</td>
-                  <td>Pequeña Demanda (hasta 25 kW-mes) en Baja Tensión</td>
+                  <td>Enero</td>
+                  <td>
+                    <svg xmlns="http://www.w3.org/2000/svg" 
+                     fill="none" 
+                     viewBox="0 0 24 24" 
+                     stroke-width="1.5" 
+                     stroke="currentColor" 
+                     class="icon-large">
+                      <path stroke-linecap="round" stroke-linejoin="round" 
+                      d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                    </svg>
+                  </td>
+
                   <td>
                     <div className="d-flex actions justify-content-around">
                     <Stack direction="row" spacing={1}>
@@ -66,8 +80,18 @@ const Tabla_GDBT = () => {
                   </td>
                 </tr>
                 <tr className="active-row">
-                  <td>GDBT</td>
-                  <td>Gran Demanda (mayor a 25 kW-mes) en Baja Tensión	</td>
+                  <td>Febrero</td>
+                  <td>
+                    <svg xmlns="http://www.w3.org/2000/svg" 
+                     fill="none" 
+                     viewBox="0 0 24 24" 
+                     stroke-width="1.5" 
+                     stroke="currentColor" 
+                     class="icon-large">
+                      <path stroke-linecap="round" stroke-linejoin="round" 
+                      d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                    </svg>
+                  </td>
                   <td>
                     <div className="d-flex actions justify-content-around">
                       <Stack direction="row" spacing={1}>
@@ -94,8 +118,18 @@ const Tabla_GDBT = () => {
                   </td>
                 </tr>
                 <tr>
-                  <td>GDMTH</td>
-                  <td>Gran Demanda en Media Tensión horaria</td>
+                  <td>Marzo</td>
+                  <td>
+                    <svg xmlns="http://www.w3.org/2000/svg" 
+                     fill="none" 
+                     viewBox="0 0 24 24" 
+                     stroke-width="1.5" 
+                     stroke="currentColor" 
+                     class="icon-large">
+                      <path stroke-linecap="round" stroke-linejoin="round" 
+                      d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                    </svg>
+                  </td>
                   <td>
                     <div className="d-flex actions justify-content-around">
                       <Stack direction="row" spacing={1}>
@@ -122,8 +156,18 @@ const Tabla_GDBT = () => {
                   </td>
                 </tr>
                 <tr className="active-row">
-                  <td>GDMTO</td>
-                  <td>Gran Demanda en Media Tensión ordinaria	</td>
+                  <td>Abril</td>
+                  <td>
+                    <svg xmlns="http://www.w3.org/2000/svg" 
+                     fill="none" 
+                     viewBox="0 0 24 24" 
+                     stroke-width="1.5" 
+                     stroke="currentColor" 
+                     class="icon-large">
+                      <path stroke-linecap="round" stroke-linejoin="round" 
+                      d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                    </svg>
+                  </td>
                   <td>
                     <div className="d-flex actions justify-content-around">
                       <Stack direction="row" spacing={1}>
@@ -146,6 +190,7 @@ const Tabla_GDBT = () => {
                           </IconButton>
                         </Link>
                       </Stack>
+
                     </div>
                   </td>
                 </tr>
@@ -154,6 +199,7 @@ const Tabla_GDBT = () => {
           </div>
         </div>
       </div>
+      
     </div>
   );
 };
--- app/src/pages/Facturas/PDBT/index.js
+++ app/src/pages/Facturas/PDBT/index.js
@@ -10,10 +10,11 @@ import Tooltip from "@mui/material/Tooltip";
 
 const Tabla_PDBT = () => {
   return (
+    
     <div className="w-100 right-content">
       <div className="card">
         <div className="table-header d-flex justify-content-end align-items-center mt-2 mb-3 p-2">
-          <Link to="/agregar_tarifa">{ /* editar para agregar tarifa */}
+          <Link to="/agregar_Factura">{ /* editar para agregar tarifa */}
             <Button
               variant="contained"
               className=" mx-2"
@@ -25,25 +26,37 @@ const Tabla_PDBT = () => {
           </Link>
         </div>
 
+
         <div className="d-flex justify-content-center align-items-center">
           <div className="table-container">
             <table className="styled-table text-center">
               <thead>
                 <tr>
                   <th colSpan="3" className="table-title text-center">
-                    Tabla de Tarifas
+                    Facturas 2019
                   </th>
                 </tr>
                 <tr>
-                  <th className="table-title text-center">Tarifa</th>
-                  <th className="table-title text-center">Descripción</th>
+                  <th className="table-title text-center">Mes</th>
+                  <th className="table-title text-center">Archivo</th>
                   <th className="table-title text-center">Acciones</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td>PDBT</td>
-                  <td>Pequeña Demanda (hasta 25 kW-mes) en Baja Tensión</td>
+                  <td>Enero</td>
+                  <td>
+                    <svg xmlns="http://www.w3.org/2000/svg" 
+                     fill="none" 
+                     viewBox="0 0 24 24" 
+                     stroke-width="1.5" 
+                     stroke="currentColor" 
+                     class="icon-large">
+                      <path stroke-linecap="round" stroke-linejoin="round" 
+                      d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                    </svg>
+                  </td>
+
                   <td>
                     <div className="d-flex actions justify-content-around">
                     <Stack direction="row" spacing={1}>
@@ -66,8 +79,18 @@ const Tabla_PDBT = () => {
                   </td>
                 </tr>
                 <tr className="active-row">
-                  <td>GDBT</td>
-                  <td>Gran Demanda (mayor a 25 kW-mes) en Baja Tensión	</td>
+                  <td>Febrero</td>
+                  <td>
+                    <svg xmlns="http://www.w3.org/2000/svg" 
+                     fill="none" 
+                     viewBox="0 0 24 24" 
+                     stroke-width="1.5" 
+                     stroke="currentColor" 
+                     class="icon-large">
+                      <path stroke-linecap="round" stroke-linejoin="round" 
+                      d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                    </svg>
+                  </td>
                   <td>
                     <div className="d-flex actions justify-content-around">
                       <Stack direction="row" spacing={1}>
@@ -94,8 +117,18 @@ const Tabla_PDBT = () => {
                   </td>
                 </tr>
                 <tr>
-                  <td>GDMTH</td>
-                  <td>Gran Demanda en Media Tensión horaria</td>
+                  <td>Marzo</td>
+                  <td>
+                    <svg xmlns="http://www.w3.org/2000/svg" 
+                     fill="none" 
+                     viewBox="0 0 24 24" 
+                     stroke-width="1.5" 
+                     stroke="currentColor" 
+                     class="icon-large">
+                      <path stroke-linecap="round" stroke-linejoin="round" 
+                      d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                    </svg>
+                  </td>
                   <td>
                     <div className="d-flex actions justify-content-around">
                       <Stack direction="row" spacing={1}>
@@ -122,8 +155,18 @@ const Tabla_PDBT = () => {
                   </td>
                 </tr>
                 <tr className="active-row">
-                  <td>GDMTO</td>
-                  <td>Gran Demanda en Media Tensión ordinaria	</td>
+                  <td>Abril</td>
+                  <td>
+                    <svg xmlns="http://www.w3.org/2000/svg" 
+                     fill="none" 
+                     viewBox="0 0 24 24" 
+                     stroke-width="1.5" 
+                     stroke="currentColor" 
+                     class="icon-large">
+                      <path stroke-linecap="round" stroke-linejoin="round" 
+                      d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                    </svg>
+                  </td>
                   <td>
                     <div className="d-flex actions justify-content-around">
                       <Stack direction="row" spacing={1}>
@@ -146,6 +189,7 @@ const Tabla_PDBT = () => {
                           </IconButton>
                         </Link>
                       </Stack>
+
                     </div>
                   </td>
                 </tr>
@@ -154,6 +198,7 @@ const Tabla_PDBT = () => {
           </div>
         </div>
       </div>
+      
     </div>
   );
 };
